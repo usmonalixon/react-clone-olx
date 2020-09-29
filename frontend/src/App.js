03a56eee5894f74c51e31a50daa973d985ada007
usmonalixon <usmonalixonnizomov@gmail.com>
2020-09-29 12:46:42 +0500

    ads-list-react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,57 @@
 import React from "react";
 import "./App.css";
+import data from "./data";
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  };
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  };
   return (
     <div className="grid-container">
       <div className="header">
-        <a className="title" href="/">
-          OLX
-        </a>
+        <div className="brand">
+          <button onClick={openMenu}>&#9776;</button>
+          <a href="/">OLX</a>
+        </div>
         <div className="header-links">
           <a href="/signin">Tizimga Kirish</a>
           <a href="/register">Ro'yhatdan o'tish</a>
           <a href="/cart">Savat</a>
         </div>
       </div>
+      <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>
+          x
+        </button>
+        <ul className="categories">
+          <li>
+            <a hrefto="/category/Pants">Pants</a>
+          </li>
 
+          <li>
+            <a href="/category/Shirts">Shirts</a>
+          </li>
+        </ul>
+      </aside>
       <div className="main">
         <div className="content">
           <ul className="ads">
-            <li>
-              <div className="ad">
-                <img className="ad-image" src="images/d1.jpg" alt="ad"></img>
-                <div className="ad-name">
-                  <a href="product.html">1 Xonali Novostroyka</a>
+            {data.products.map((product) => (
+              <li>
+                <div className="ad">
+                  <img className="ad-image" src={product.image} alt="ad"></img>
+                  <div className="ad-name">
+                    <a href="product.html">{product.name}</a>
+                  </div>
+                  <div className="ad-sort">{product.sort}</div>
+                  <div className="ad-price">{product.price}</div>
                 </div>
-                <div className="ad-sort">1 Xonalik</div>
-                <div className="ad-price">8 500 000 so'm</div>
-              </div>
-            </li>
-            <li>
-              <div className="ad">
-                <img className="ad-image" src="images/d1.jpg" alt="ad"></img>
-                <div className="ad-name">
-                  <a href="product.html">1 Xonali Novostroyka</a>
-                </div>
-                <div className="ad-sort">1 Xonalik</div>
-                <div className="ad-price">8 500 000 so'm</div>
-              </div>
-            </li>
-            <li>
-              <div className="ad">
-                <img className="ad-image" src="images/d1.jpg" alt="ad"></img>
-                <div className="ad-name">
-                  <a href="product.html">1 Xonali Novostroyka</a>
-                </div>
-                <div className="ad-sort">1 Xonalik</div>
-                <div className="ad-price">8 500 000 so'm</div>
-              </div>
-            </li>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
---
+++ frontend/src/data.js
@@ -0,0 +1,46 @@
+export default  {
+  products: [
+    {
+      _id: 1,
+      image: "/images/d1.jpg",
+      name: "1 Xonalik Novostroyka",
+      sort: "1-Xonalik",
+      price: "1000000 so'm",
+    },
+    {
+      _id: 2,
+      name: "1 Xonalik Novostroyka",
+      sort: "1-Xonalik",
+      image: "/images/d1.jpg",
+      price: 1000000 + "so'm",
+    },
+    {
+      _id: 3,
+      name: "1 Xonalik Novostroyka",
+      sort: "1-Xonalik",
+      image: "/images/d1.jpg",
+      price: 1000000 + "so'm",
+    },
+    {
+      _id: 4,
+      name: "1 Xonalik Novostroyka",
+      sort: "1-Xonalik",
+      image: "/images/d1.jpg",
+      price: 1000000 + "so'm",
+    },
+    {
+      _id: 5,
+      name: "1 Xonalik Novostroyka",
+      sort: "1-Xonalik",
+      image: "/images/d1.jpg",
+      price: 1000000 + "so'm",
+    },
+    {
+      _id: 6,
+      name: "1 Xonalik Novostroyka",
+      sort: "1-Xonalik",
+      image: "/images/d1.jpg",
+      price: 1000000 + "so'm",
+    },
+  ],
+};
