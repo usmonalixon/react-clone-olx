c069c56d1b00b5b644b69a9707273938bbbc8870
usmonalixon <usmonalixonnizomov@gmail.com>
2020-10-07 12:49:33 +0500

    BACKEND-PART

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
-import Ads from "./components/Ads";
+import AdsScreen from "./screens/AdsScreen";
 
 function App() {
   const openMenu = () => {
@@ -14,17 +14,18 @@ function App() {
   return (
     <BrowserRouter>
       <div className="grid-container">
-        <div className="header">
+        <header className="header">
           <div className="brand">
             <button onClick={openMenu}>&#9776;</button>
             <Link to="/">OLX</Link>
           </div>
           <div className="header-links">
+            TEL: 909900290
             <Link to="/signin">Tizimga Kirish</Link>
             <Link to="/register">Ro'yhatdan o'tish</Link>
             <Link to="/cart">Savat</Link>
           </div>
-        </div>
+        </header>
         <aside className="sidebar">
           <h3>Shopping Categories</h3>
           <button className="sidebar-close-button" onClick={closeMenu}>
@@ -32,20 +33,22 @@ function App() {
           </button>
           <ul className="categories">
             <li>
-              <Link to="/category/Pants">Pants</Link>
+              <Link to="/category/1-xonalik">Pants</Link>
             </li>
-
             <li>
-              <Link to="/category/Shirts">Shirts</Link>
+              <Link to="/category/2-xonalik">Shirts</Link>
             </li>
           </ul>
         </aside>
-        <div className="main">
+        <main className="main">
           <div className="content">
             <Route path="/" exact={true} component={HomeScreen} />
+            <Route path="/ad/:id" component={AdsScreen} />
           </div>
-        </div>
-        <div className="footer">All Rights Reserved</div>
+        </main>
+        <footer className="footer">
+          All Rights reserved@. Tel: 909900290. Telegram: Usmon.uz
+        </footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,40 @@
-import React, { Component } from "react";
-import Ads from "../components/Ads";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Fade from 'react-reveal/Fade';
 
 function HomeScreen(props) {
+  const [ads, setAd] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/ads");
+      setAd(data);
+    };
+    fetchData();
+    return () => {
+      //
+    };
+  }, []);
   return (
-    <div>
-      <Ads></Ads>
-    </div>
+    <Fade cascade bottom>
+      <ul className="ads">
+        {ads.map((ad) => (
+          <li>
+            <div className="ad">
+              <Link to={"/ad/" + ad._id}>
+                <img className="ad-image" src={ad.image} alt="ad" />
+              </Link>
+              <div className="ad-name">
+                <Link to={"/ad/" + ad._id}>{ad.name}</Link>
+              </div>
+              <div className="ad-sort">{ad.sort}</div>
+              <div className="ad-price">${ad.price}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </Fade>
   );
 }
 export default HomeScreen;
